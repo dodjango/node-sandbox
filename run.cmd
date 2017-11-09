bcc61659aea40e35d3767b69af69debae02af19e
Torsten Mattetat <torsten.mattetat@inform-ac.com>
2017-11-09 23:59:41 +0100

    fix: typo

--- run.cmd
+++ run.cmd
@@ -6,7 +6,7 @@ docker run^
   -e "NODE_ENV=production"^
   -u "node"^
   -m "300M" --memory-swap "1G"^
-  -w "/use/src/app"^
+  -w "/usr/src/app"^
   --name "sandbox"^
   -v "%CD%":/usr/src/app^
   sandbox^
