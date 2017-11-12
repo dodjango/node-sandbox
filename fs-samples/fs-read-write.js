6ca5e3705bc6df55ecf64851e3d962f2304ab6a9
Torsten Mattetat <torsten.mattetat@inform-ac.com>
2017-11-12 20:47:47 +0100

    doc: source code comments

--- fs-samples/fs-read-write.js
+++ fs-samples/fs-read-write.js
@@ -2,12 +2,16 @@
   "use strict";
 
   const fs = require('fs');
+  // work with a text file named as this script and add extension
   const filename = __filename + '.txt';
 
+  // remove file if it exists
   if (fs.existsSync(filename)) {
     fs.unlinkSync(filename);
   }
+  // write some content to the file
   fs.writeFileSync(filename, 'hello world');
+  // read content agan and write it to the console
   fs.readFile(filename, function (err, data) {
     if (err) {
       console.error(err.toString());
