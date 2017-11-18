f76c3ef81bcb1f37332097f87fb362db3105c1e2
Torsten Mattetat <torsten.mattetat@inform-ac.com>
2017-11-18 01:04:16 +0100

    feat: add bash starter for windows

---
+++ bash.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+REM *** start container attached
+docker run^
+  -it^
+  --rm^
+  -e "NODE_ENV=production"^
+  -u "node"^
+  -m "300M" --memory-swap "1G"^
+  -w "/usr/src/app"^
+  --name "node-sandbox"^
+  -v "%CD%":/usr/src/app^
+  -p 49160:8080^
+  dodjango/node-sandbox
