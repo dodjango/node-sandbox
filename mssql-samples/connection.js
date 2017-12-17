c857c6bb709b0767d70a4804b440f6f68e7bf7ca
Torsten Mattetat <derTorsten@planetdjango.de>
2017-12-17 23:38:50 +0100

    fix program termination

--- mssql-samples/connection.js
+++ mssql-samples/connection.js
@@ -1,32 +1,35 @@
-"use strict";
+(() => {
+  "use strict";
 
-const sql = require('mssql');
+  const sql = require('mssql');
 
-const config = {
-  user: 'sa',
-  password: 'sql2017Playground!',
-  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
-  database: 'master',
-  
-  options: {
-    encrypt: false // Use this if you're on Windows Azure
-  }
-};
-
-(async function () {
-  try {
-    let pool = await sql.connect(config)
-    let result1 = await pool.request()
-    .input('input_parameter', sql.NVarChar, "test")
-    .query("select name from sys.databases where 'test' = @input_parameter");
+  const config = {
+    user: 'sa',
+    password: 'sql2017Playground!',
+    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+    database: 'master',
     
-    console.dir(result1);
+    options: {
+      encrypt: false // Use this if you're on Windows Azure
+    }
+  };
 
-  } catch (err) {
+  (async function () {
+    try {
+      let pool = await sql.connect(config)
+      let result1 = await pool.request()
+      .input('input_parameter', sql.NVarChar, "test")
+      .query("select name from sys.databases where 'test' = @input_parameter");
+
+      console.dir(result1);
+      pool.close(); // necessary, otherwise the program does not end due to referenced connections
+    } catch (err) {
+      console.log(err);
+    }
+  })();
+
+
+  sql.on('error', err => {
     console.log(err);
-  }
+  });
 })();
-
-sql.on('error', err => {
-  console.log(err);
-});
