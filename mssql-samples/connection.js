17ad6a6c535d6717edd0dbc8aecf95ac3cff229f
Torsten Mattetat <derTorsten@planetdjango.de>
2017-12-18 00:02:25 +0100

    add error handling

--- mssql-samples/connection.js
+++ mssql-samples/connection.js
@@ -14,22 +14,32 @@
     }
   };
 
-  (async function () {
+  sql.on('error', err => {
+    console.log("SQL ERROR:");
+    console.log(err);
+  });
+  sql.on('info', info => {
+    console.log("SQL INFO:");
+    console.dir(info)
+  });
+
+  (async () => {
+    let pool;
     try {
-      let pool = await sql.connect(config)
+      pool = await sql.connect(config)
       let result1 = await pool.request()
       .input('input_parameter', sql.NVarChar, "test")
-      .query("select name from sys.databases where 'test' = @input_parameter");
+      .query("select name from sys.databases where 'test' = @input_parameter")
+//      .query("RAISERROR (N'This is message %s %d.', 15, 1, N'number', 5)");
 
       console.dir(result1);
       pool.close(); // necessary, otherwise the program does not end due to referenced connections
     } catch (err) {
+      pool.close(); // necessary, otherwise the program does not end due to referenced connections
+      console.log("ERROR:");
       console.log(err);
     }
   })();
 
 
-  sql.on('error', err => {
-    console.log(err);
-  });
 })();
